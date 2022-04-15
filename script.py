029c545c413251e68ef6cc119cad437c3ccf8427
kcfutaba <kcfutaba@bu.edu>
2022-04-15 18:59:06 -0400

    Update script.py
    Script

--- script.py
+++ script.py
@@ -16,58 +16,64 @@ data_frame = pd.DataFrame(df)
 #file = 't.csv'
 #open(file, 'wb').write(content)
 
+# Count the number of fake and real news
 df_1 = df.groupby(['label'])['label'].count()
 
-x = ['Fake', 'Real']
-y = []
-y.append(df_1[0])
-y.append(df_1[1])
+x1 = ['Fake', 'Real']
+y1 = []
+y1.append(df_1[0])
+y1.append(df_1[1])
 
 c = ['red', 'blue']
 
-
-plt.bar(x, y, color=c)
+# Make a bar graph for total number of fake and real news
+plt.bar(x1, y1, color=c)
 plt.title("Total Number of Fake and Real News")
 plt.xlabel("Fake/Real")
 plt.ylabel("Number of fakes and reals")
 plt.show()
 
-
+# Extract data for fake news from a label column
 df_fake =df.loc[df['label'] == "FAKE", ['text']]
 df_fake['text'] = df_fake['text'].str.lower()
 df_fake
 
+# Count word in the text of fake news
 df_fake["text_new"] = df_fake['text'].str.lower().str.replace('[^\w\s]','')
 new_df_1 = df_fake.text_new.str.split(expand=True).stack().value_counts().reset_index()
 new_df_1.columns = ['Word', 'Frequency']
 new_df_1
 
-x = []
-y = []
-y.append(new_df_1[0])
-y.append(new_df_1[1])
+# Make a scatter plot for the word frequency in the text of fake news
+x2 = []
+y2 = []
+y2.append(new_df_1[0])
+y2.append(new_df_1[1])
 
-plt.scatter.plot(x, y)
+plt.scatter.plot(x2, y2)
 plt.title("The frequency of words for fake news")
 plt.xlabel("Word")
 plt.ylabel("Frequency")
 plt.show()
 
+# Extract data for real news from a label column
 df_real = df.loc[df['label'] == "REAL", ['text']]
 df_real['text'] = df_real['text'].str.lower()
 df_real
 
+# Count word in the text of real news
 df_real["text_new"] = df_real['text'].str.lower().str.replace('[^\w\s]','')
 new_df_2 = df_real.text_new.str.split(expand=True).stack().value_counts().reset_index()
 new_df_2.columns = ['Word', 'Frequency']
 new_df_2
 
-x = []
-y = []
-y.append(new_df_2[0])
-y.append(new_df_2[1])
+# Make a scatter plot for the word frequency in the text of real news
+x3 = []
+y3 = []
+y3.append(new_df_2[0])
+y3.append(new_df_2[1])
 
-plt.scatter.plot(x, y)
+plt.scatter.plot(x3, y3)
 plt.title("The frequency of words for real news")
 plt.xlabel("Word")
 plt.ylabel("Frequency")
