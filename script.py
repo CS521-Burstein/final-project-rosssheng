3f7945c2ca43797ab700abe359a829412a6d7d18
rosssheng <ross62@bu.edu>
2022-04-26 23:02:41 -0400

    4/27 deliverable

--- script.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import requests
-CSV_URL = "https://github.com/bursteinalan/Data-Sets/raw/master/Fake%20News/news.csv"
-df = pd.read_csv(CSV_URL)
-
-data_frame = pd.DataFrame(df)
-
-
